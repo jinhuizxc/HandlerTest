36f994068280a4228e44a2068fdab13f7944cfa1
jinhuizxc <1004260403@qq.com>
2018-11-17 17:24:59 +0800

    更新UI的4种方式

---
+++ app/src/main/java/com/example/jinhui/handlertest/FiveActivity.java
@@ -0,0 +1,84 @@
+package com.example.jinhui.handlertest;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Email: 1004260403@qq.com
+ * Created by jinhui on 2018/11/17.
+ *
+ * 更新UI的4种方式
+ */
+public class FiveActivity extends AppCompatActivity {
+
+    TextView textView;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            textView.setText("ok");
+        }
+    };
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five);
+
+        textView = findViewById(R.id.textView);
+
+        // 方式一
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(1000);
+//                    handler1();  // 方式一
+//                    handler2();   // 方式二
+//                    updateUI();   // 方式三
+                    viewUI();   // 方式四
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    private void viewUI() {
+       textView.post(new Runnable() {
+           @Override
+           public void run() {
+               textView.setText("ok");
+           }
+       });
+    }
+
+    private void updateUI() {
+      runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+              textView.setText("ok");
+          }
+      });
+    }
+
+    private void handler2() {
+        handler.sendEmptyMessage(1);
+    }
+
+    // 方式一
+    private void handler1(){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText("ok");
+            }
+        });
+    }
+}
