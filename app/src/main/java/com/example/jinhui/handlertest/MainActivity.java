b39298d1a8bcd3d4068fd726b2bce46988dea6af
jinhuizxc <1004260403@qq.com>
2018-11-17 15:19:53 +0800

    callback消息截获

--- app/src/main/java/com/example/jinhui/handlertest/MainActivity.java
+++ app/src/main/java/com/example/jinhui/handlertest/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.jinhui.handlertest;
 
+import android.annotation.SuppressLint;
 import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * handler
@@ -19,8 +25,47 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    Handler handler = new Handler();
+//    Handler handler = new Handler();
+    Button bt_remove;
 
+    // 测试3，发送消息
+//    @SuppressLint("HandlerLeak")
+//    Handler handler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+////            textView.setText(msg.arg1 + "-" + msg.arg2);
+//            textView.setText(msg.obj + "");
+//        }
+//    };
+
+    // 测试4 callBack
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            Toast.makeText(getApplicationContext(), "" + 1, Toast.LENGTH_SHORT).show();
+            return true;  // 补充: false 不截获消息，弹出toast 1-2， true截获消息 弹出1，不弹出2
+        }
+    }){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(getApplicationContext(), "" + 2, Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    class Person{
+        public String name;
+        public int age;
+
+        @NonNull
+        @Override
+        public String toString() {
+            return "name = " + name + " " + "age = " + age;
+        }
+    }
     ImageView imageView;
 
     private int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
@@ -45,11 +90,49 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
         imageView = findViewById(R.id.img);
+        bt_remove = findViewById(R.id.button);
 
-        // 测试2
-//        handler.postDelayed(myRunnable, 1000);
+        // 移除handler
+        bt_remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                handler.removeCallbacks(myRunnable);
+                // 测试callBack
+                handler.sendEmptyMessage(1);
+            }
+        });
 
-        // 测试1
+        // 测试3
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+//                    Message message = new Message();
+//                    message.arg1 = 88;
+//                    message.arg2 = 100;
+                    // 复用系统的message对象
+                    Message message = handler.obtainMessage();
+
+                    // 发送大量的数据时；
+                    Person person = new Person();
+                    person.name = "jinhui";
+                    person.age = 16;
+                    message.obj = person;
+//                    handler.sendMessage(message);
+                    message.sendToTarget();  // 通过message发送消息
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+
+        // 测试2 图片轮播
+        handler.postDelayed(myRunnable, 1000);
+
+        // 测试1 更新UI
 //        new Thread(new Runnable() {
 //            @Override
 //            public void run() {
